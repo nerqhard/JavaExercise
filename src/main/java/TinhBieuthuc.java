840b74dbda507b4f8ecb596656b4221834047d7b
QuangHa <quangha12thxd@gmail.com>
2019-05-16 00:01:48 +0700

    Remove method giaithua

--- src/main/java/TinhBieuthuc.java
+++ src/main/java/TinhBieuthuc.java
@@ -1,16 +1,11 @@
 public class TinhBieuthuc {
 
-    public static long giaithua(int a) {
-        long gt = 1;
-        for (int i = 1; i<=a; i++) {
-            gt *= i;
-        }
-        return gt;
-    }
     public double bthuc(int n){
         double s = 1;
-        for (int j = 1; j <= n; j++) {
-            s += 1.0/giaithua(j);
+        double x = 1;
+        for (int i = 1; i <= n; i++) {
+            x /= i;
+            s += x;
         }
         return s;
     }
