2f548e5232993eeb6f8f3edf61acf147e84f038a
QuangHa <quangha12thxd@gmail.com>
2019-05-15 11:21:16 +0700

    Update while thanh for

--- src/main/java/Tongdayso.java
+++ src/main/java/Tongdayso.java
@@ -3,11 +3,8 @@ public class Tongdayso {
 
     public int tdso(int n) {
         int tong = 0;
-        int s1 = 1;
-
-        while (n >= s1) {
+        for (int s1 = 1; s1 <= n; s1++) {
             tong += s1*(s1+1);
-            s1++;
         }
         return tong;
     }
