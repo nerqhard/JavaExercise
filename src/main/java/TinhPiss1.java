e08006990f084c9ed4c26fd087d5b5c8aa8a4c2b
QuangHa <quangha12thxd@gmail.com>
2019-05-15 22:03:36 +0700

    Update dieu kien cua While

--- src/main/java/TinhPiss1.java
+++ src/main/java/TinhPiss1.java
@@ -1,14 +1,14 @@
 public class TinhPiss1 {
 
     public double tinhpiss1 () {
-        int n = 0;
-        double pi = 0;
+        int n = 1;
+        double pi = 4;
         double s;
         do {
-            s = 1.0/(n*2 + 1);
+            s = 4.0/(n*2 + 1);
             pi += Math.pow(-1,n)*s;
             n ++;
-        } while (s > 0.00001);
-        return pi*4;
+        } while (s >= 0.0001);
+        return pi;
     }
 }
