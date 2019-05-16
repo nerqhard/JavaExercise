d575193641c84da32b96308c263d2f218683f364
QuangHa <quangha12thxd@gmail.com>
2019-05-16 20:26:44 +0700

    Remove Math.pow()

--- src/main/java/TinhPi2.java
+++ src/main/java/TinhPi2.java
@@ -5,8 +5,12 @@ public class TinhPi2 {
         double x;
         int  n = 1;
         do {
-            x = 4.0/(2*n*(2*n+1)*(2*n+2));
-            pi += Math.pow(-1,n+1)*x;
+            x = 4.0 /(2 * n * (2 * n+1) * (2 * n + 2));
+            if (n % 2 == 0) {
+                pi -= x;
+            } else {
+                pi += x;
+            }
             n ++;
         } while (x >= 0.00001);
         return pi;
--- src/test/java/TinhPi2Test.java
+++ src/test/java/TinhPi2Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class TinhPi2Test {
 
     @Test
-    public void tinhpi2() {
+    public void test_tinhpi2() {
         assertEquals(new TinhPi2().tinhpi2(),3.14159,0.00001);
     }
 }
