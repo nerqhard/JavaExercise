a5b52a238ea73f3e84be22e7e2fdc0925d0aa03e
QuangHa <quangha12thxd@gmail.com>
2019-05-16 20:36:55 +0700

    Update add space

--- src/main/java/TinhBieuthuc.java
+++ src/main/java/TinhBieuthuc.java
@@ -1,6 +1,6 @@
 public class TinhBieuthuc {
 
-    public double bthuc(int n){
+    public double bieuthuc(int n){
         double s = 1;
         double x = 1;
         for (int i = 1; i <= n; i++) {
--- src/test/java/TinhBieuthucTest.java
+++ src/test/java/TinhBieuthucTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.*;
 public class TinhBieuthucTest {
 
     @Test
-    public void test_bthuc() {
-        assertEquals(new TinhBieuthuc().bthuc(5),2.717,0.001);
-        assertEquals(new TinhBieuthuc().bthuc(0),1,0.001);
-        assertEquals(new TinhBieuthuc().bthuc(-5),1,0.001);
+    public void test_bieuthuc() {
+        assertEquals(new TinhBieuthuc().bieuthuc(5),2.717,0.001);
+        assertEquals(new TinhBieuthuc().bieuthuc(0),1,0.001);
+        assertEquals(new TinhBieuthuc().bieuthuc(-5),1,0.001);
     }
 }
