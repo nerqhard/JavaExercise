786427db9c204e121123075e6f3311385a01d84c
QuangHa <quangha12thxd@gmail.com>
2019-05-18 00:06:04 +0700

    Bo sung them Tam giac deu

--- src/main/java/Toadotgiac.java
+++ src/main/java/Toadotgiac.java
@@ -1,9 +1,12 @@
 public class Toadotgiac {
 
     public String tdtgiac(double x1, double y1, double x2, double y2, double x3, double y3) {
-        double a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-        double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-        double c = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+        double a;
+        double b;
+        double c;
+        a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+        c = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
         if ((a + b > c) && (a + c > b) && (b + c > a)) {
             double x = (a + b + c) / 2;
             double s = Math.sqrt(x * (x - a) * (x - b) * (x - c));
@@ -13,7 +16,11 @@ public class Toadotgiac {
                 if ((a == b) || (b == c) || (a == c)) {
                     return String.format("Tam giac can. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
                 } else {
-                    return String.format("Tam giac thuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                    if ((a == b) && (b == c)) {
+                        return String.format("Tam giac deu. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                    } else {
+                        return String.format("Tam giac thuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                    }
                 }
             }
         } else {
