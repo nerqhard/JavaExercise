95d0dc544b7e230a74e1c6b22054f0640a1dbf9a
QuangHa <quangha12thxd@gmail.com>
2019-05-17 11:39:41 +0700

    Bai 17: Tim chu so

---
+++ src/main/java/Timchuso.java
@@ -0,0 +1,30 @@
+public class Timchuso {
+
+    public static int demcs(int a) {
+        int count = 0;
+        while (a >0) {
+            a /= 10;
+            count ++;
+        }
+        return count;
+    }
+
+    public String timcso() {
+        StringBuffer kq = new StringBuffer();
+        for (int i = 10; i <= 99999; i++) {
+            int s = 0;
+            int m = demcs(i);
+            int a = i % 10;
+            int b = i;
+            while (b > 0){
+                s += Math.pow(a,m);
+                b /= 10;
+                a = b % 10;
+            }
+            if (s == i) {
+                kq.append(i + " ");
+            }
+        }
+        return kq.toString();
+    }
+}
