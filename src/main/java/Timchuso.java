c03200e02e25f16e16c652286adede68cfb60155
QuangHa <quangha12thxd@gmail.com>
2019-05-17 13:27:48 +0700

    Remove dau cach o cuoi ket qua

--- src/main/java/Timchuso.java
+++ src/main/java/Timchuso.java
@@ -2,13 +2,12 @@ public class Timchuso {
 
     public static int demcs(int a) {
         int count = 0;
-        while (a >0) {
+        while (a > 0) {
             a /= 10;
             count ++;
         }
         return count;
     }
-
     public String timcso() {
         StringBuffer kq = new StringBuffer();
         for (int i = 10; i <= 99999; i++) {
@@ -17,12 +16,16 @@ public class Timchuso {
             int a = i % 10;
             int b = i;
             while (b > 0){
-                s += Math.pow(a,m);
+                s += Math.pow(a, m);
                 b /= 10;
                 a = b % 10;
             }
             if (s == i) {
-                kq.append(i + " ");
+                if (kq.length() == 0) {
+                    kq.append(i);
+                } else {
+                    kq.append(" " + i);
+                }
             }
         }
         return kq.toString();
