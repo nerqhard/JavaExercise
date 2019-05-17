c42b733550dc6fec7e11558e0e40cccad621518d
QuangHa <quangha12thxd@gmail.com>
2019-05-17 22:19:06 +0700

    Doi phuong phap bai 18

--- src/main/java/DaoSoNhiPhan.java
+++ src/main/java/DaoSoNhiPhan.java
@@ -1,16 +1,10 @@
 public class DaoSoNhiPhan {
 
     public int daoso(int n) {
-        int bin[] = new int[30];
-        int a = 0;
         int s = 0;
         while (n > 0) {
-            bin[a] = n % 2;
+            s = s * 2 + n % 2;
             n /= 2;
-            a++;
-        }
-        for (int i = 0; i < a; i++) {
-            s +=  bin[a - i - 1] * Math.pow(2, i);
         }
         return s;
     }
