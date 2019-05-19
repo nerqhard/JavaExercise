f561325f2f946960f6b75756b63dcc2ff8ab6d4e
Võ Quang Hòa <hvquang@openwt.com>
2019-05-19 12:17:58 +0700

    Refactoring

--- src/main/java/Toadotgiac.java
+++ src/main/java/Toadotgiac.java
@@ -1,25 +1,31 @@
 public class Toadotgiac {
 
+
+    private double getLength(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
     public String tdtgiac(double x1, double y1, double x2, double y2, double x3, double y3) {
-        double a;
-        double b;
-        double c;
-        a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-        b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-        c = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+
+        double a = getLength(x1, y1, x2, y2);
+        double b = getLength(x1, y1, x3, y3);
+        double c = getLength(x2, y2, x3, y3);
+
         if ((a + b > c) && (a + c > b) && (b + c > a)) {
-            double x = (a + b + c) / 2;
-            double s = Math.sqrt(x * (x - a) * (x - b) * (x - c));
+            double p = (a + b + c) / 2;
+
+            double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
             if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b)) {
-                return String.format("Tam giac vuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                return String.format("Tam giac vuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * p);
             } else {
                 if ((a == b) || (b == c) || (a == c)) {
-                    return String.format("Tam giac can. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                    return String.format("Tam giac can. Dien tich: %.2f. Chu vi: %.2f", s, 2 * p);
                 } else {
                     if ((a == b) && (b == c)) {
-                        return String.format("Tam giac deu. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                        return String.format("Tam giac deu. Dien tich: %.2f. Chu vi: %.2f", s, 2 * p);
                     } else {
-                        return String.format("Tam giac thuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * x);
+                        return String.format("Tam giac thuong. Dien tich: %.2f. Chu vi: %.2f", s, 2 * p);
                     }
                 }
             }
