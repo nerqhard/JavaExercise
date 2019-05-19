20b133cda5c09d3aa7432543e9ec6331d99f2c3e
QuangHa <quangha12thxd@gmail.com>
2019-05-19 21:49:10 +0700

    Xoa phan chu thich

--- src/main/java/TamgiacPascal.java
+++ src/main/java/TamgiacPascal.java
@@ -15,28 +15,4 @@ public class TamgiacPascal {
         }
         return array;
     }
-
 }
-
-    /*
-    public static int combine(int n, int k) {
-        if (k > n) {
-            return 0;
-        } else {
-            if (k == 0 || k == n) {
-                return 1;
-            } else {
-                return (combine(n - 1, k) + combine(n - 1, k - 1));
-            }
-        }
-    }
-    public String tgpascal(int n) {
-        StringBuffer kq = new StringBuffer();
-        for (int i = 0; i < n; i++) {
-                kq.append(combine(n - 1, i) + " ");
-        }
-        return kq.toString().trim();
-    }
-}
-     */
-
