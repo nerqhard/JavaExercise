4f76b6bb816d6d9a528c51afed5b650a0355a34e
QuangHa <quangha12thxd@gmail.com>
2019-05-20 20:52:24 +0700

    Fix theo Review 2

--- src/main/java/Timchuso.java
+++ src/main/java/Timchuso.java
@@ -4,27 +4,28 @@ public class Timchuso {
         int count = 0;
         while (a > 0) {
             a /= 10;
-            count ++;
+            count++;
         }
         return count;
     }
-    public static int valid (int n) {
-        int s = 0;
+
+    public static boolean valid(int n) {
+        int sum = 0;
         int m = demcs(n);
-        int a = n % 10;
         int b = n;
-        while (b > 0){
-            s += Math.pow(a, m);
+        while (b > 0) {
+            int a = b % 10;
+            sum += Math.pow(a, m);
             b /= 10;
-            a = b % 10;
         }
-        return s;
+        return sum == n;
     }
+
     public String timcso() {
         StringBuffer kq = new StringBuffer();
         for (int i = 10; i <= 99999; i++) {
-            if (valid(i) == i) {
-                    kq.append(i + " ");
+            if (valid(i)) {
+                kq.append(i + " ");
             }
         }
         return kq.toString().trim();
--- src/test/java/TimchusoTest.java
+++ src/test/java/TimchusoTest.java
@@ -6,6 +6,6 @@ public class TimchusoTest {
 
     @Test
     public void timcso() {
-        assertEquals(new Timchuso().timcso(),"153 370 371 407 1634 8208 9474 54748 92727 93084");
+        assertEquals(new Timchuso().timcso(), "153 370 371 407 1634 8208 9474 54748 92727 93084");
     }
 }
