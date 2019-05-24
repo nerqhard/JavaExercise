544d1be3ad79b46b4f7092d699e3d19a3c15630d
QuangHa <quangha12thxd@gmail.com>
2019-05-24 10:13:51 +0700

    Fix theo PP dung Map

--- src/main/java/DecimaltoRomanNumber.java
+++ src/main/java/DecimaltoRomanNumber.java
@@ -1,60 +1,37 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class DecimaltoRomanNumber {
 
-    public String Dec_to_Roman(int input) {
-        if (input < 1 || input > 3999)
+    public String dec_to_roman(int input) {
+        if (input < 1 || input > 3999) {
             return "Invalid Roman Number Value";
+        }
+        TreeMap<Integer, String> hmap = new TreeMap<>(Comparator.reverseOrder());
+        hmap.put(1000, "M");
+        hmap.put(900, "CM");
+        hmap.put(500, "D");
+        hmap.put(400, "CD");
+        hmap.put(100, "C");
+        hmap.put(90, "XC");
+        hmap.put(50, "L");
+        hmap.put(40, "XL");
+        hmap.put(10, "X");
+        hmap.put(9, "IX");
+        hmap.put(5, "V");
+        hmap.put(4, "IV");
+        hmap.put(1, "I");
         String s = "";
-        while (input >= 1000) {
-            s += "M";
-            input -= 1000;
-        }
-        while (input >= 900) {
-            s += "CM";
-            input -= 900;
-        }
-        while (input >= 500) {
-            s += "D";
-            input -= 500;
-        }
-        while (input >= 400) {
-            s += "CD";
-            input -= 400;
-        }
-        while (input >= 100) {
-            s += "C";
-            input -= 100;
-        }
-        while (input >= 90) {
-            s += "XC";
-            input -= 90;
-        }
-        while (input >= 50) {
-            s += "L";
-            input -= 50;
-        }
-        while (input >= 40) {
-            s += "XL";
-            input -= 40;
-        }
-        while (input >= 10) {
-            s += "X";
-            input -= 10;
-        }
-        while (input >= 9) {
-            s += "IX";
-            input -= 9;
-        }
-        while (input >= 5) {
-            s += "V";
-            input -= 5;
-        }
-        while (input >= 4) {
-            s += "IV";
-            input -= 4;
-        }
-        while (input >= 1) {
-            s += "I";
-            input -= 1;
+
+        for (Map.Entry<Integer, String> sethmap : hmap.entrySet()) {
+            int key = sethmap.getKey();
+            if (input / key != 0) {
+                for (int i = 0; i < input / key; i++) {
+                    s += hmap.get(key);
+                }
+                input %= key;
+            }
         }
         return s;
     }
--- src/test/java/DecimaltoRomanNumberTest.java
+++ src/test/java/DecimaltoRomanNumberTest.java
@@ -6,9 +6,9 @@ public class DecimaltoRomanNumberTest {
 
     @Test
     public void dec_to_Roman() {
-        assertEquals(new DecimaltoRomanNumber().Dec_to_Roman(1511), "MDXI");
-        assertEquals(new DecimaltoRomanNumber().Dec_to_Roman(1982), "MCMLXXXII");
-        assertEquals(new DecimaltoRomanNumber().Dec_to_Roman(0), "Invalid Roman Number Value");
-        assertEquals(new DecimaltoRomanNumber().Dec_to_Roman(5000), "Invalid Roman Number Value");
+        assertEquals(new DecimaltoRomanNumber().dec_to_roman(1511), "MDXI");
+        assertEquals(new DecimaltoRomanNumber().dec_to_roman(1982), "MCMLXXXII");
+        assertEquals(new DecimaltoRomanNumber().dec_to_roman(0), "Invalid Roman Number Value");
+        assertEquals(new DecimaltoRomanNumber().dec_to_roman(5000), "Invalid Roman Number Value");
     }
 }
