fe14b59f7c280b36a1ccdb6cb24b85bb22639d21
QuangHa <quangha12thxd@gmail.com>
2019-05-28 16:49:03 +0700

    Tong so nguyen to

--- src/main/java/array/Ex5_Tongsonguyento.java
+++ src/main/java/array/Ex5_Tongsonguyento.java
@@ -1,4 +1,32 @@
 package array;
 
 public class Ex5_Tongsonguyento {
+    public static boolean nguyento(int n) {
+        if (n < 2) {
+            return false;
+        } else {
+            if (n > 2) {
+                if (n % 2 == 0) {
+                    return false;
+                } else {
+                    for (int i = 3; i < (float) Math.sqrt(n); i++) {
+                        if (n % i == 0) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    public int tong_so_nto(int []a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (nguyento(a[i])) {
+                sum += a[i];
+            }
+        }
+        return sum;
+    }
 }
--- src/test/java/arraytest/Ex5_TongsonguyentoTest.java
+++ src/test/java/arraytest/Ex5_TongsonguyentoTest.java
@@ -1,5 +1,22 @@
+package arraytest;
+
+import array.Ex5_Tongsonguyento;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Ex5_TongsonguyentoTest {
 
+    @Test
+    public void nguyento() {
+        assertEquals(new Ex5_Tongsonguyento().nguyento(1), false);
+        assertEquals(new Ex5_Tongsonguyento().nguyento(2), true);
+        assertEquals(new Ex5_Tongsonguyento().nguyento(5), true);
+        assertEquals(new Ex5_Tongsonguyento().nguyento(18), false);
+    }
+
+    @Test
+    public void tong_so_nto() {
+        assertEquals(new Ex5_Tongsonguyento().tong_so_nto(new int[]{2, 1, 5, 10, 55, 77, 16}), 7);
+    }
 }
