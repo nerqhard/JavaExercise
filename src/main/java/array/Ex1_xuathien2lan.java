2894974f86c4be3667aea7918553eaa3e93e8166
QuangHa <quangha12thxd@gmail.com>
2019-05-28 13:32:27 +0700

    Fix lai PP

--- src/main/java/array/Ex1_xuathien2lan.java
+++ src/main/java/array/Ex1_xuathien2lan.java
@@ -1,22 +1,21 @@
 package array;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Ex1_xuathien2lan {
 
     public List<Integer> xh_hon_2_lan(int[] a) {
         List<Integer> result = new ArrayList<>();
-        Arrays.sort(a);
-        int count = 0;
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] == a[i - 1]) {
-                count++;
-            }
-            if (((a[i] != a[i - 1]) && (count > 1)) || ((i == (a.length - 1)) && (count > 1))) {
-                result.add(a[i - 1]);
-                count = 0;
+        for (int i = 0; i < a.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] == a[i]) {
+                    count++;
+                }
+                if ((count > 1) && (!result.contains(a[i]))) {
+                    result.add(a[i]);
+                }
             }
         }
         return result;
--- src/test/java/arraytest/Ex1_xuathien2lanTest.java
+++ src/test/java/arraytest/Ex1_xuathien2lanTest.java
@@ -11,7 +11,9 @@ public class Ex1_xuathien2lanTest {
 
     @Test
     public void test_xh_hon_2_lan() {
-        assertEquals(new Ex1_xuathien2lan().xh_hon_2_lan(new int[]{2, 3, 4, 1, 4, 2, 2}), Arrays.asList(2));
+        assertEquals(new Ex1_xuathien2lan().xh_hon_2_lan(new int[]{2, 4, 1, 4, 2, 2}), Arrays.asList(2));
         assertEquals(new Ex1_xuathien2lan().xh_hon_2_lan(new int[]{1, 1, 2, 2, 2, 1, 1,}), Arrays.asList(1, 2));
+        assertEquals(new Ex1_xuathien2lan().xh_hon_2_lan(new int[]{2, 2, 1, 1, 1, 2, 1,}), Arrays.asList(2, 1));
+        assertEquals(new Ex1_xuathien2lan().xh_hon_2_lan(new int[]{2, 3, 1, 1, 1, 2, 1, 3, 3, 2}), Arrays.asList(2, 3, 1));
     }
 }
