6d5d858d705f27a8a33a73581821288e5707c5a6
QuangHa <quangha12thxd@gmail.com>
2019-06-03 22:57:20 +0700

    Fix Review 1

--- src/main/java/array/Ex4_sapxepchanle.java
+++ src/main/java/array/Ex4_sapxepchanle.java
@@ -1,25 +1,23 @@
 package array;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Ex4_sapxepchanle {
 
-    public ArrayList<Integer> sx_chanle(int[] a) {
-        ArrayList<Integer> chan = new ArrayList<>();
-        ArrayList<Integer> le = new ArrayList<>();
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0) {
-                chan.add(a[i]);
-            } else {
-                le.add(a[i]);
+    public int[] sx_chanle(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] % 2 == 1 && a[j] % 2 == 0) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+                if (((a[i] + a[j]) % 2 == 0) && ((a[i] > a[j]) == (a[i] % 2 < 1))) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
-        Collections.sort(chan);
-        Collections.sort(le);
-        Collections.reverse(le);
-        chan.addAll(le);
-        return chan;
+        return a;
     }
 }
