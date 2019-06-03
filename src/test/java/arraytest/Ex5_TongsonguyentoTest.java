e7c746250c99f1489b8fb4ec18aa52cd54df645e
QuangHa <quangha12thxd@gmail.com>
2019-06-03 22:39:17 +0700

    Fix Review 1

--- src/test/java/arraytest/Ex5_TongsonguyentoTest.java
+++ src/test/java/arraytest/Ex5_TongsonguyentoTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class Ex5_TongsonguyentoTest {
 
     @Test
-    public void nguyento() {
+    public void test_nguyento() {
         assertEquals(new Ex5_Tongsonguyento().nguyento(1), false);
         assertEquals(new Ex5_Tongsonguyento().nguyento(2), true);
         assertEquals(new Ex5_Tongsonguyento().nguyento(5), true);
@@ -16,7 +16,7 @@ public class Ex5_TongsonguyentoTest {
     }
 
     @Test
-    public void tong_so_nto() {
+    public void test_tong_so_nto() {
         assertEquals(new Ex5_Tongsonguyento().tong_so_nto(new int[]{2, 1, 5, 10, 55, 77, 16}), 7);
     }
 }
