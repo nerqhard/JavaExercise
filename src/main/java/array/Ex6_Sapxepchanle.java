cfad38b486f726e9649b1a2a0c29d88125db0fa7
QuangHa <quangha12thxd@gmail.com>
2019-06-03 23:56:54 +0700

    Fix Review 1

--- src/main/java/array/Ex6_Sapxepchanle.java
+++ src/main/java/array/Ex6_Sapxepchanle.java
@@ -6,7 +6,7 @@ public class Ex6_Sapxepchanle {
         for (int i = 1; i < a.length; i++) {
             for (int j = 1; j < a.length; j++) {
                 if ((a[i] + a[j]) % 2 == 0) {
-                    if (((a[i] % 2 == 0) && (a[i] < a[j])) || ((a[i] % 2 == 1) && (a[i] > a[j]))) {
+                    if ((a[i] % 2 == 0) == (a[i] < a[j])) {
                         int temp = a[i];
                         a[i] = a[j];
                         a[j] = temp;
