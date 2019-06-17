ad90db610b776a89e675fddded5135dd689ee104
QuangHa <quangha12thxd@gmail.com>
2019-06-17 21:25:23 +0700

    Arr Bai 27: Mang so Max

--- src/main/java/array/Ex27_Mang_somax.java
+++ src/main/java/array/Ex27_Mang_somax.java
@@ -1,4 +1,16 @@
 package array;
 
+import java.util.Arrays;
+
 public class Ex27_Mang_somax {
+    public int[] mang_max(int[] a, int n) {
+        int[] result = new int[n];
+        int id = 0;
+        Arrays.sort(a);
+        for (int i = a.length - 1; i >= a.length - n; i--) {
+            result[id] = a[i];
+            id++;
+        }
+        return result;
+    }
 }
--- src/test/java/arraytest/Ex27_Mang_somaxTest.java
+++ src/test/java/arraytest/Ex27_Mang_somaxTest.java
@@ -1,5 +1,13 @@
-import static org.junit.Assert.*;
+package arraytest;
+
+import array.Ex27_Mang_somax;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Ex27_Mang_somaxTest {
 
+    @Test
+    public void mang_max() {
+        Assert.assertArrayEquals(new Ex27_Mang_somax().mang_max(new int[]{3, 4, 5, 8, 1, 6, 9, 7, 5}, 4), new int[]{9, 8, 7, 6});
+    }
 }
