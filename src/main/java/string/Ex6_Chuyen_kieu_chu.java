d0436b100538dca971077887fa8ca4899cf6162c
QuangHa <quangha12thxd@gmail.com>
2019-06-24 22:12:07 +0700

    Str Bai 6: Fix String thanh StringBuilder

--- src/main/java/string/Ex6_Chuyen_kieu_chu.java
+++ src/main/java/string/Ex6_Chuyen_kieu_chu.java
@@ -2,14 +2,14 @@ package string;
 
 public class Ex6_Chuyen_kieu_chu {
     public String cover_word(String str) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             if (Character.isUpperCase(str.charAt(i))) {
-                result += String.valueOf(str.charAt(i)).toLowerCase();
+                result.append(String.valueOf(str.charAt(i)).toLowerCase());
             } else {
-                result += String.valueOf(str.charAt(i)).toUpperCase();
+                result.append(String.valueOf(str.charAt(i)).toUpperCase());
             }
         }
-        return result;
+        return result.toString();
     }
 }
