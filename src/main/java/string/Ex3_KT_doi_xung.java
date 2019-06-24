be70f4243117f2d2a90adf1c8a714b4b69eb1e7a
QuangHa <quangha12thxd@gmail.com>
2019-06-24 22:30:21 +0700

    Str Bai 3: Fix 1

--- src/main/java/string/Ex3_KT_doi_xung.java
+++ src/main/java/string/Ex3_KT_doi_xung.java
@@ -1,12 +1,8 @@
 package string;
 
 public class Ex3_KT_doi_xung {
-    public boolean str_mirror (String str) {
+    public boolean str_mirror(String str) {
         String temp = new StringBuffer(str).reverse().toString();
-        if (str.equals(temp)) {
-            return true;
-        } else {
-            return false;
-        }
+        return str.equals(temp);
     }
 }
