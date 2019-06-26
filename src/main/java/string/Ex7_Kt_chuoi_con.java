96c7af6753269dcca0f89aecb3e4f4c6bc2f5952
QuangHa <quangha12thxd@gmail.com>
2019-06-26 22:45:02 +0700

    Str Bai 7: Fix 1

--- src/main/java/string/Ex7_Kt_chuoi_con.java
+++ src/main/java/string/Ex7_Kt_chuoi_con.java
@@ -4,8 +4,11 @@ public class Ex7_Kt_chuoi_con {
     public int check_sub_str(String str, String text) {
         int id = 0;
         for (int i = 0; i < str.length(); i++) {
-            while ((id < text.length()) && (str.charAt(i) == text.charAt(id))) {
+            if ((id < text.length()) && (str.charAt(i) == text.charAt(id))) {
                 id++;
+            } else {
+                id = 0;
+                continue;
             }
             if (id == text.length()) {
                 return i - id + 1;
