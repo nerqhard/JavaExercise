c5bc1dcde7ade68acd18e26321248b826bc6ed09
Võ Quang Hòa <voquanghoa@gmail.com>
2019-07-01 10:06:29 +0700

    Update Ex7_Kt_chuoi_conTest.java

--- src/test/java/stringtest/Ex7_Kt_chuoi_conTest.java
+++ src/test/java/stringtest/Ex7_Kt_chuoi_conTest.java
@@ -10,9 +10,11 @@ public class Ex7_Kt_chuoi_conTest {
 
     @Test
     public void test_check_sub_str() {
-        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "uang H"),8);
-        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "Nguy"),0);
-        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "uang h"),-1);
-        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguye n", "Nguyen"),-1);
+        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "uang H"), 8);
+        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "Nguy"), 0);
+        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguyen Quang Ha", "uang h"), -1);
+        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("Nguye n", "Nguyen"), -1);
+        Assert.assertEquals(new Ex7_Kt_chuoi_con().check_sub_str("NNguyen", "Nguyen"), 1);
+
     }
 }
