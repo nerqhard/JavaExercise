01a166db065e2f7b0e588482e2a8ea84482715d8
QuangHa <quangha12thxd@gmail.com>
2019-07-01 22:22:56 +0700

    Arr Bai 21: Fix 2

--- src/test/java/arraytest/Ex21_Dinhthuc_matrixTest.java
+++ src/test/java/arraytest/Ex21_Dinhthuc_matrixTest.java
@@ -7,6 +7,18 @@ import org.junit.Test;
 public class Ex21_Dinhthuc_matrixTest {
 
     @Test
+    public void test_minor() {
+        Assert.assertArrayEquals(new Ex21_Dinhthuc_matrix().minor(new double[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }, 2), new double[][]{
+                {4, 5},
+                {7, 8}
+        });
+    }
+
+    @Test
     public void det_matrix() {
         Assert.assertEquals(new Ex21_Dinhthuc_matrix().det_matrix(new double[][]{
                 {1, 5, 6, 2},
