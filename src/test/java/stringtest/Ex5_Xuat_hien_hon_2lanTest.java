3a0009d5c3b46e08e99188181a8090f768c6aa24
Võ Quang Hòa <voquanghoa@gmail.com>
2019-07-01 10:16:58 +0700

    Fail case

--- src/test/java/stringtest/Ex5_Xuat_hien_hon_2lanTest.java
+++ src/test/java/stringtest/Ex5_Xuat_hien_hon_2lanTest.java
@@ -11,6 +11,6 @@ public class Ex5_Xuat_hien_hon_2lanTest {
         Assert.assertEquals(new Ex5_Xuat_hien_hon_2lan().xh_2lan("haha hihi hihi hoho haha hehe haha hihi kaka"), "haha, hihi");
         Assert.assertEquals(new Ex5_Xuat_hien_hon_2lan().xh_2lan("a a a a b b c b"), "a, b");
         Assert.assertEquals(new Ex5_Xuat_hien_hon_2lan().xh_2lan("a a a a"), "a");
-
+        Assert.assertEquals(new Ex5_Xuat_hien_hon_2lan().xh_2lan("a"), "");
     }
 }
