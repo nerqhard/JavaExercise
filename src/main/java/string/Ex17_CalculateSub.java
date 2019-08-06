abc1660f32515ccdfdaed0c78e09c511836599c9
QuangHa <quangha12thxd@gmail.com>
2019-08-06 20:20:20 +0700

    Fix Exercise 17 1

--- src/main/java/string/Ex17_CalculateSub.java
+++ src/main/java/string/Ex17_CalculateSub.java
@@ -8,41 +8,31 @@ public class Ex17_CalculateSub {
         return (a.compareTo(b) < 0);
     }
 
+    private int numberFromRight(String str, int i) {
+        if (i >= str.length()) {
+            return 0;
+        }
+        return str.charAt(str.length() - 1 - i) - '0';
+    }
+
     public String calSub(String a, String b) {
-        String neg = "";
         if (isSmaller(a, b)) {
-            String temp = a;
-            a = b;
-            b = temp;
-            neg = "-";
+            return "-" + calSub(b, a);
         }
-        StringBuilder str = new StringBuilder();
-        int n1 = a.length();
-        int n2 = b.length();
-        a = new StringBuilder(a).reverse().toString();
-        b = new StringBuilder(b).reverse().toString();
+        StringBuilder sb = new StringBuilder();
         int carry = 0;
-        for (int i = 0; i < n2; i++) {
-            int sub = ((a.charAt(i) - '0') - (b.charAt(i) - '0') - carry);
+        for (int i = 0; i < a.length(); i++) {
+            int sub = (numberFromRight(a, i) - numberFromRight(b, i) - carry);
             if (sub < 0) {
                 sub = sub + 10;
                 carry = 1;
             } else
                 carry = 0;
-            str.append(sub);
+            sb.append(sub);
         }
-        for (int i = n2; i < n1; i++) {
-            int sub = (a.charAt(i) - '0' - carry);
-            if (sub < 0) {
-                sub = sub + 10;
-                carry = 1;
-            } else
-                carry = 0;
-            str.append(sub);
+        while (sb.charAt(sb.length() - 1) == '0') {
+            sb.delete(sb.length() - 1, sb.length());
         }
-        while (str.charAt(str.length() - 1) == '0') {
-            str.delete(str.length() - 1, str.length());
-        }
-        return (str.reverse()).insert(0,neg).toString();
+        return sb.reverse().toString();
     }
 }
