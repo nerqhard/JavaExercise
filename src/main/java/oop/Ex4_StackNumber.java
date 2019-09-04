7c648368ac9cd3fbc863ef2ec68c5ddc8b39b7b4
QuangHa <quangha12thxd@gmail.com>
2019-09-04 22:11:40 +0700

    OOP Exercise 4: StackNumber

---
+++ src/main/java/oop/Ex4_StackNumber.java
@@ -0,0 +1,26 @@
+package oop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex4_StackNumber<T> {
+
+    private List<T> stackNumber = new ArrayList<>();
+
+    public void push(T num) {
+        stackNumber.add(num);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        T result = stackNumber.get(stackNumber.size() - 1);
+        stackNumber.remove(result);
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return stackNumber.isEmpty();
+    }
+}
